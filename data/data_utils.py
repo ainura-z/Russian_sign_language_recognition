d59fd4f9df82c294c3969ac77b065e344d9c46a8
unknown <cmon86559@gmail.com>
2023-10-28 09:36:41 +0300

    add augm

--- data/data_utils.py
+++ data/data_utils.py
@@ -151,3 +151,190 @@ class LandmarksDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.video_paths)
+    
+
+class LandmarksTestDataset(torch.utils.data.Dataset):
+    """Dataset of landmarks"""
+    def __init__(self, landmarks, file_names, annot, label_enc, seq_len):
+        self.video_paths = file_names
+        # sample videos to be of length seq_len (if len(video) > seq_len)
+        self.landmarks = {file:sample_video(landmarks[file], seq_len) for file in self.video_paths}
+        self.seq_len = seq_len
+
+        # label encoding of labels
+        self.labels = []
+        self.sizes = []
+        for filename in self.video_paths:
+            ann = annot[annot['attachment_id']==filename].iloc[0]
+            self.sizes.append((ann['height'], ann['width']))
+            self.labels.append(label_enc[ann['text']])
+        
+    def __getitem__(self, idx):
+        filename = self.video_paths[idx]
+        landmarks = self.landmarks[filename]
+        label = self.labels[idx]
+        # size of frame
+        H, W = self.sizes[idx]
+        hands_order = ['hand 1', 'hand 2']
+        points = []
+        for i, frame in enumerate(landmarks):
+            curr_landm = []
+            # for each hand
+            for key in hands_order:
+                if key in frame:
+                    curr_frame = frame[key]
+                    # frame keypoints
+                    curr_coord = [[point['x'], point['y'], point['z']] for point in curr_frame]
+                    curr_coord = np.array(curr_coord)
+                    curr_landm.extend(curr_coord)
+                else:
+                    # replace missing landmarks with 0
+                    curr_landm.extend(np.zeros((21, 3)).astype(np.float32)) 
+            points.append(curr_landm)
+        points = np.array(points).reshape(len(landmarks), -1)
+
+        # padding of sequences to be of length seq_len
+        padding = (self.seq_len - len(landmarks))
+        # add padding
+        if padding > 0:
+            padd_points = np.zeros((padding, points.shape[-1]))
+            points = np.concatenate((points, padd_points))
+        
+        return torch.from_numpy(points).float(), torch.tensor(label).long()
+
+    def __len__(self):
+        return len(self.video_paths)
+    
+def get_maxmin_limits(points, impute_val=-10):
+    minx = points[:, :, 0][points[:, :, 0]>impute_val].min()
+    maxx = points[:, :, 0][points[:, :, 0]>impute_val].max()
+    miny = points[:, :, 1][points[:, :, 1]>impute_val].min()
+    maxy = points[:, :, 1][points[:, :, 1]>impute_val].max()
+    return minx, maxx, miny, maxy
+
+def shift_transform(coords, left_limit, right_limit, top_limit, bottom_limit, H, W):
+    power = np.random.uniform(0.05, 0.3)
+    left_limit, right_limit = int((1-power)*left_limit), min(int((1+power)*right_limit), W-1)
+    top_limit, bottom_limit = int((1-power)*top_limit), min(int((1+power)*bottom_limit), H-1)
+    
+    shift_x = np.random.randint(-left_limit, W - right_limit, size=1)[0]
+    shift_y = np.random.randint(-top_limit, H - bottom_limit, size=1)[0]
+    
+    coords[..., 0] = coords[..., 0] + shift_x
+    coords[..., 1] = coords[..., 1] + shift_y
+    return coords, shift_x, shift_y
+
+def crop_transform(coords, left_limit, right_limit, top_limit, bottom_limit, H, W):
+    power = np.random.uniform(0.1, 0.5)
+    left_limit, right_limit = int((1-power)*left_limit), min(int((1+power)*right_limit), W-1)
+    top_limit, bottom_limit = int((1-power)*top_limit), min(int((1+power)*bottom_limit), H-1)
+    
+    if left_limit > 0:
+        crop_left = np.random.randint(0, left_limit, size=1)[0]
+    else:
+        crop_left = 0
+    crop_right = np.random.randint(0, W-right_limit, size=1)[0]
+    if top_limit > 0:
+        crop_top = np.random.randint(0, top_limit, size=1)[0]
+    else:
+        crop_top = 0
+    crop_bottom = np.random.randint(0, H-bottom_limit, size=1)[0]
+
+    coords[..., 0] = coords[..., 0] - crop_left
+    coords[..., 1] = coords[..., 1] - crop_top
+    
+    crops = (crop_left, crop_right, crop_top, crop_bottom)
+    new_H = H-crop_bottom-crop_top
+    new_W = W-crop_left-crop_right
+    return coords, crops, new_H, new_W
+
+def pad_ifneeded(coords, H, W, min_size=700):
+    new_H, new_W = H, W
+    
+    if min(H, W) < min_size:
+        padd = min_size - min(H, W)
+        padd = padd + padd % 2
+        
+        if W < H:
+            coords[..., 0] = coords[..., 0] + (padd // 2)
+            new_W = W + padd
+        elif H < W:
+            coords[..., 1] = coords[..., 1] + (padd // 2)
+            new_H = H + padd
+    return coords, new_H, new_W
+
+class LandmarksDataset2(torch.utils.data.Dataset):
+    """Dataset of landmarks"""
+    def __init__(self, landmarks, file_names, annot, label_enc, seq_len):
+        self.video_paths = file_names
+        # sample videos to be of length seq_len (if len(video) > seq_len)
+        self.landmarks = {file:sample_video(landmarks[file], seq_len) for file in self.video_paths}
+        self.seq_len = seq_len
+
+        # label encoding of labels
+        self.labels = []
+        self.sizes = []
+        for filename in self.video_paths:
+            ann = annot[annot['attachment_id']==filename].iloc[0]
+            self.sizes.append((ann['height'], ann['width']))
+            self.labels.append(label_enc[ann['text']])
+        
+    def __getitem__(self, idx):
+        filename = self.video_paths[idx]
+        landmarks = self.landmarks[filename]
+        label = self.labels[idx]
+        # size of frame
+        H, W = self.sizes[idx]
+        hands_order = ['hand 1', 'hand 2']
+        
+        points = []
+        for i, frame in enumerate(landmarks):
+            curr_landm = []
+            # for each hand
+            for key in hands_order:
+                if key in frame:
+                    curr_frame = frame[key]
+                    # frame keypoints
+                    curr_coord = [[point['x'], point['y'], point['z']] for point in curr_frame]
+                    curr_coord = np.array(curr_coord)
+                    curr_landm.extend(curr_coord)
+                else:
+                    # replace missing landmarks with 0
+                    curr_landm.extend(np.full((21, 3), -100000).astype(np.float32)) 
+            points.append(curr_landm)
+        # num_frames x seq_len x 3
+        points = np.array(points)#.reshape(len(landmarks), -1)
+
+        p_shift = 0.5
+        p_crop = 0.5
+        p_pad = 0.5
+
+        abs_points = get_absolute_coord(points.copy(), H, W, clip=False)
+        minx, maxx, miny, maxy = get_maxmin_limits(abs_points)
+        coords = abs_points.copy()
+        new_H, new_W = H, W
+
+        if np.random.rand() > p_shift:
+            coords, shx, shy = shift_transform(coords.copy(), minx, maxx, miny, maxy, H, W)
+            minx, maxx, miny, maxy = minx+shx, maxx+shx, miny+shy, maxy+shy 
+        if np.random.rand() > p_crop:
+            coords, crops, new_H, new_W = crop_transform(coords.copy(), minx, maxx, miny, maxy, H, W)
+            minx, maxx, miny, maxy = get_maxmin_limits(coords)
+        if np.random.rand() > p_pad:
+            coords, new_H, new_W = pad_ifneeded(coords.copy(), new_H, new_W)
+
+        final_coords = get_relative_coord(coords.astype('float'), new_H, new_W)
+        final_coords[..., 2] = np.where(points[..., 2] > -5, points[..., 2], 0.0)
+        final_coords = final_coords.reshape(len(landmarks), -1)
+        
+        # padding of sequences to be of length seq_len
+        padding = (self.seq_len - len(landmarks))
+        # add padding
+        if padding > 0:
+            padd_points = np.zeros((padding, final_coords.shape[-1]))
+            final_coords = np.concatenate((final_coords, padd_points))
+        
+        return torch.from_numpy(final_coords).float(), torch.tensor(label).long()
+
+    def __len__(self):
+        return len(self.video_paths)
